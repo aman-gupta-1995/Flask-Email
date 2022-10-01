3924209eeda6db183db9285f96f2fbe11a2ebf46
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-02 04:07:02 +0530

    Create setup.py

--- Flask-MailGun-develop/setup.py
+++ Flask-MailGun-develop/setup.py
@@ -8,7 +8,7 @@ for sending and receving emails
 try: # for pip >= 10   
     from pip._internal.req import parse_requirements
 except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+    from pip.req import parse_requirements 
 from io import open
 try:
     from setuptools import setup
