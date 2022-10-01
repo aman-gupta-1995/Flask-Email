b33d8fa606033d1e2a1625ecc6e79ce48bc29093
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-02 04:07:07 +0530

    Update setup.py

--- Flask-MailGun-develop/setup.py
+++ Flask-MailGun-develop/setup.py
@@ -58,6 +58,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ] 
 )
 
