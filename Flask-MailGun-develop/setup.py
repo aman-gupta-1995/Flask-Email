3b2ff8df0acf5a11a76a1d713d63e3a6dc261931
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-02 04:07:12 +0530

    Update setup.py

--- Flask-MailGun-develop/setup.py
+++ Flask-MailGun-develop/setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Flask-MailGun
-Flask extension to use the Mailgun email parsing service 
+Flask extension to use the Mailgun email parsing service  
 for sending and receving emails 
 """     
 try: # for pip >= 10   
