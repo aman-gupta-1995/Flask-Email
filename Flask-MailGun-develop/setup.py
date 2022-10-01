a9948df6f60892411b7a4c6402dceab311fb43cc
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-10-02 04:09:30 +0530

    Create setup.py

--- Flask-MailGun-develop/setup.py
+++ Flask-MailGun-develop/setup.py
@@ -6,7 +6,7 @@ Flask extension to use the Mailgun email parsing service
 for sending and receving emails    
 """             
 try: # for pip >= 10         
-    from pip._internal.req import parse_requirements       
+    from pip._internal.req import parse_requirements        
 except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements 
 from io import open
